f6c43291563eeba20f3ceab69a2134d7d2985834
Lucas Klein <lk.lucasklein@gmail.com>
2024-07-08 19:19:22 -0300

    removendo pasta

--- INFmon code/lib/charactions.h
+++
@@ -1,5 +0,0 @@
-int moveObjetoParaDireita(int posX, int length);
-int moveObjetoParaEsquerda(int posX, int largura);
-int moveObjetoParaCima(int posY, int altura);
-int moveObjetoParaBaixo(int posY, int altura);
-
--- INFmon code/lib/utils.h
+++
@@ -1,8 +0,0 @@
-#include "raylib.h"
-
-Color defineCorDoBotao (int indexBotao, int mouseHoverRec);
-void abreModalDeConfirmacao(char texto[], int tamanhoFonte, float margin, int alturaModal, int larguraModal, int larguraTela, int alturaTela, int* isAbertaModal);
-Rectangle criaRetangulo(float posX, float posY, float width, float height, float gap, int nRetangulo);
-Rectangle criaBotaoDeModal(float posInicialX, float posInicialY, float posFinalX, float posFinalY, Color corFundo, int larguraBorda);
-Rectangle abreModal(float posX, float posY, float width, float height, float sceenWidth, float screenHeight, float larguraLinha);
-Rectangle criaBotao(float posX, float posY, float width, float height, float gap, float nBotao, float larguraBorda, Color corPadrao, Color corSecundaria, int isEmCima);
