2618eaa513ff283ce258c6e6bad0bf8d03ad0a0e
Lucas Klein <lk.lucasklein@gmail.com>
2024-08-01 23:21:16 -0300

    corrigindo erro e desenhando mapa na tela

--- INFmon project/src/mapa.c
+++ INFmon project/src/mapa.c
@@ -2,42 +2,64 @@
 #include<stdio.h>
 
 #define PROCESSO_INICIA_JOGO 4
-
-char mapa[30][60] = {};
+#define MAX_LINHA 30
+#define MAX_COLUNA 61
 
 void inicializaMapa(int processoAtual){
-    FILE *arqMapa;
-    arqMapa = fopen("./mapas/mapa3.txt", "r");
+    char mapa[MAX_LINHA][MAX_COLUNA] = {};
 
-    while(!WindowShouldClose() && processoAtual == PROCESSO_INICIA_JOGO){
-        int linha,coluna;
-        char dadoColetado;
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+    FILE *arqMapa = NULL;
+    arqMapa = fopen("testFile.txt", "r");
 
-        if(arqMapa == NULL){
-            printf("\n\tARQUIVO INEXISTENTE\n");
-        } else {
-            linha = 0;
-            coluna = 0;
+    char dadoColetado;
 
-            while(!feof(arqMapa)){
+    if(arqMapa == NULL){
+        printf("\n\tARQUIVO INEXISTENTE\n");
+    } else {
+        for(int i = 0; i < MAX_LINHA; i++){
+            for(int j = 0; j < MAX_COLUNA; j++){
                 dadoColetado = fgetc(arqMapa);
-//                printf("DADO COLETADO = %c\n",dadoColetado);
-
-                if(dadoColetado == '\n'){
-                    printf("\n");
-//                    linha = 0;
-//                    coluna++;
-                } else {
-                    printf("%c", dadoColetado);
-//                    mapa[linha][coluna] = dadoColetado;
+                if(dadoColetado != '\n'){
+                    mapa[i][j] = dadoColetado;
                 }
             }
         }
+        fclose(arqMapa);
 
-        DrawText("TESTEEEEEEEE", 10, 10, 50, BLACK);
-        EndDrawing();
+        while(!WindowShouldClose() && processoAtual == PROCESSO_INICIA_JOGO){
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            desenhaMapa(mapa);
+
+            EndDrawing();
+        }
     }
+}
 
+void desenhaMapa(char mapa[MAX_LINHA][MAX_COLUNA]){
+    Color corPosMapa = WHITE;
+
+    for(int i = 0; i < MAX_LINHA; i++){
+        for(int j = 0; j < MAX_COLUNA -1 ; j++){
+            switch(mapa[i][j]){
+                case 'W':
+                    corPosMapa = BLACK;
+                break;
+                case 'G':
+                    corPosMapa = GREEN;
+                break;
+                case ' ':
+                    corPosMapa = WHITE;
+                break;
+                case 'J':
+                    corPosMapa = BLUE;
+                break;
+                case 'E':
+                    corPosMapa = RED;
+                break;
+            }
+            DrawRectangle(j*20, i*20, 20, 20, corPosMapa);
+        }
+    }
 }
